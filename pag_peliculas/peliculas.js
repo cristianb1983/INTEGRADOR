6f3d63450b0137931b08280c7721c85f83e5693f
Rodrigo Bordon <rodrigozbordon@gmail.com>
2025-10-20 18:57:54 +0200

    Carrusel pruebas

--- pag_peliculas/peliculas.js
+++ pag_peliculas/peliculas.js
@@ -1,5 +1,12 @@
 let imagenes = document.getElementsByClassName("poster");
-console.log(imagenes);
-function miFuncion() {
-    console.log("Si funciono")
+let posicion = 5;
+function miFuncion1() {
+    for(let i = 0; i < imagenes.length; i++){
+        if(posicion == 0){
+            return
+        }
+        imagenes[i].innerHTML = imagenes[posicion].innerHTML
+        posicion--;
+    }
 }
+
